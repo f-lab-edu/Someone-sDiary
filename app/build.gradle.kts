4d946334ba62736cf4c56954bcdcb6cf3db2c073
Tom <blak1002@gmail.com>
2023-12-09 20:35:13 +0900

    [config(app:gradle)] recyclerview 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     //KSP
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.20-1.0.14")
     //jetpack-navigation
