d18e31cb8d1f77df817893802d8a8b388686d94b
Tom <blak1002@gmail.com>
2023-12-12 10:14:17 +0900

    [config(app:build.gradle)] OkHttp-logging-interceptor의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    //OkHttp-logging-interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
