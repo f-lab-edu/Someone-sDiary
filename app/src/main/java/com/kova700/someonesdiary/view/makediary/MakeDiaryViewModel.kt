14018d5dcca1176dcba6aee078fad43a9b19d1e6
Tom <blak1002@gmail.com>
2023-12-13 23:23:10 +0900

    [refector] 검색어 없으면 검색 되지 않게 수정

--- app/src/main/java/com/kova700/someonesdiary/view/makediary/MakeDiaryViewModel.kt
+++ app/src/main/java/com/kova700/someonesdiary/view/makediary/MakeDiaryViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kova700.someonesdiary.data.source.unsplash.UnsplashRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
@@ -15,10 +17,13 @@ class MakeDiaryViewModel @Inject constructor(
     private val unsplashRepository: UnsplashRepository
 ) : ViewModel() {
 
+    //TODO : 쿼리 실패 예외처리
     val currentQuery = MutableStateFlow("")
     private val currentPageFlow: MutableStateFlow<Int> = MutableStateFlow(1)
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     val photoListFlow = currentQuery
+        .filter { query -> query.isNotBlank() }
         .flatMapLatest { query ->
             unsplashRepository.getPhotoList(
                 query = query
