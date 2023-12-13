b5730e912faf5d97484ba5a85ba9a87469b11839
Tom <blak1002@gmail.com>
2023-12-13 22:48:46 +0900

    [refector(PhotoResponse)] 프로젝트에 필요하지 않은 json 필드 삭제

--- app/src/main/java/com/kova700/someonesdiary/data/source/unsplash/remote/PhotoResponse.kt
+++ app/src/main/java/com/kova700/someonesdiary/data/source/unsplash/remote/PhotoResponse.kt
@@ -13,52 +13,9 @@ data class PhotoResponse(
 @Serializable
 data class Photo(
     val id: String,
-    @SerialName("blur_hash") val blurHash: String,
-    @SerialName("created_at") val createdAt: String,
-    @SerialName("current_user_collections") val currentUserCollections: List<CurrentUserCollection>,
-    @SerialName("liked_by_user") val likedByUser: Boolean,
-    val description: String,
-    val color: String,
     val height: Int,
     val width: Int,
-    val likes: Int,
-    val links: Links,
-    val urls: Urls,
-    val user: User
-)
-
-@Serializable
-data class CurrentUserCollection(
-    @SerialName("cover_photo") val coverPhoto: CoverPhoto? = null,
-    val id: Int,
-    @SerialName("last_collected_at") val lastCollectedAt: String,
-    @SerialName("published_at") val publishedAt: String,
-    val title: String,
-    @SerialName("updated_at") val updatedAt: String,
-    val user: User? = null
-)
-
-@Serializable
-data class CoverPhoto(
-    @SerialName("blur_hash") val blurHash: String,
-    val color: String,
-    val description: String,
-    val height: Int,
-    val id: String,
-    @SerialName("liked_by_user") val likedByUser: Boolean,
-    val likes: Int,
-    val links: Links,
-    val urls: Urls,
-    val user: User,
-    val width: Int
-)
-
-
-@Serializable
-data class Links(
-    val download: String,
-    val html: String,
-    val self: String
+    val urls: Urls
 )
 
 @Serializable
@@ -67,34 +24,5 @@ data class Urls(
     val raw: String,
     val regular: String,
     val small: String,
-    val thumb: String
-)
-
-@Serializable
-data class User(
-    @SerialName("first_name") val firstName: String,
-    val id: String,
-    @SerialName("instagram_username") val instagramUsername: String,
-    @SerialName("last_name") val lastName: String,
-    val links: LinksX,
-    val name: String,
-    @SerialName("portfolio_url") val portfolioUrl: String,
-    @SerialName("profile_image") val profileImage: ProfileImage,
-    @SerialName("twitter_username") val twitterUsername: String,
-    val username: String
-)
-
-@Serializable
-data class LinksX(
-    val html: String,
-    val likes: String,
-    val photos: String,
-    val self: String
-)
-
-@Serializable
-data class ProfileImage(
-    val large: String,
-    val medium: String,
-    val small: String
+    val thumb: String,
 )
